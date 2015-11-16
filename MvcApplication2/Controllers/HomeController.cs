742d411598d7b14f61017eb54c713a97e031207a
rajraebl <rajraebl@gmail.com>
2015-11-16 19:22:22 +0530

    ss

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        [HandleError]
+        public ActionResult Index()
+        {
+            throw new Exception("Something happened");
+        }
+
+        //
+        // GET: /Home/Details/5
+
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+     
+    }
+}
