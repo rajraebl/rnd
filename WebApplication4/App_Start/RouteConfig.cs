18166c66c2d2260310ca7a11b4597c0bcbd4a87e
rajraebl <rajraebl@gmail.com>
2015-11-30 18:50:27 +0530

    route attribute

--- WebApplication4/App_Start/RouteConfig.cs
+++ WebApplication4/App_Start/RouteConfig.cs
@@ -9,11 +9,16 @@ namespace WebApplication4
 {
     public class RouteConfig
     {
+        //http://sampathloku.blogspot.in/2013/11/attribute-routing-with-aspnet-mvc-5.html
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //You can keep the Convention-based Routing also with the same file but routes.MapMvcAttributeRoutes(); Should configure before the Convention-based Routing
+
+            //to enable Attribute Routing of mvc 5
             routes.MapMvcAttributeRoutes();
+            //for web api above wud be config.MapHttpAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
