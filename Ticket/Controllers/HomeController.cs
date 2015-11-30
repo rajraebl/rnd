8e53c6c946dddef54a2dda62a184e58f702ab5e8
rajraebl <rajraebl@gmail.com>
2015-11-30 10:22:45 +0530

    jqueryWithpartialview

---
+++ Ticket/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ticket.Models;
+
+namespace Ticket.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var model = new Company();
+            model.Departments = new List<Department>();
+
+
+            for (int i = 1; i < 3; i++)
+            {
+                var data = new Department();
+                data.DepartmentName = "DepartmentName:" + i;
+                data.AdminName = "Admin:" + i;
+                model.Departments.Add(data);
+
+            }
+
+
+            return View(model);
+        }
+
+        //public ActionResult showPartialView()
+        //{
+        //    return View();
+        //}
+        public PartialViewResult secondPartialView()
+        {
+            return PartialView("_MySecondPartial");
+        }
+
+    }
+}
---
+++ Ticket/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ticket.Models
+{
+    public class Company
+    {
+        public List<Department> Departments { get; set; }
+    }
+
+    public class Department {
+        public string DepartmentName  { get; set; }
+        public string AdminName { get; set; }
+    }
+}
