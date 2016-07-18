b180813401d4568167aec0adc86c7ec2c48da55d
rajraebl <rajraebl@gmail.com>
2016-07-18 17:36:20 +0530

    PubSubForOddEven PRoject

---
+++ PubClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace PubClient
+{
+    class Program
+    {
+        static readonly string ConnStr = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+        static readonly string TopicName = ConfigurationManager.AppSettings["TopicName"];
+
+        static void Main(string[] args)
+        {
+            
+            InitPub();
+            StartPub();
+        }
+
+        private static void StartPub()
+        {
+            TopicClient topicClient = TopicClient.CreateFromConnectionString(ConnStr, TopicName);
+
+            while (true)
+            {
+                Console.WriteLine("Please enter number of message to send:");
+                var messageCount = Convert.ToInt16( Console.ReadLine());
+
+                for (int i = 1; i <= messageCount; i++)
+                {
+                    var msg = new BrokeredMessage("Message:" + i + " sent at:" + DateTime.Now);
+                    msg.Properties["MessageNumber"] = i;
+                    topicClient.Send(msg);
+                    Console.WriteLine("Message Sent:" + i);
+                }
+
+            }
+        }
+
+        
+
+        public static void InitPub()
+        {
+            
+            NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(ConnStr);
+
+            if (!namespaceManager.TopicExists(TopicName))
+                namespaceManager.CreateTopic(TopicName);
+        }
+    }
+}
---
+++ SubClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace SubClient
+{
+    class Program
+    {
+        private static string connStr = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+        static readonly string TopicName = ConfigurationManager.AppSettings["TopicName"];
+        static readonly string SubscriptionName = ConfigurationManager.AppSettings["SubscriptionName"];
+        static void Main(string[] args)
+        {
+            initSub();
+            startSub();
+        }
+
+        private static void startSub()
+        {
+            SubscriptionClient subscriptionClient = SubscriptionClient.CreateFromConnectionString(connStr, TopicName, SubscriptionName);
+
+            Console.WriteLine("Waiting for message to come");
+
+
+            subscriptionClient.OnMessage((message) =>
+            {
+                try
+                {
+                    Console.WriteLine("Message Received!!!!!Message Number: " + message.Properties["MessageNumber"]);
+                    Console.WriteLine("MessageID: " + message.MessageId + "///////Body: " + message.GetBody<string>());
+
+                    // Remove message from subscription.
+                    message.Complete();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Message Receiving Failed..........!");
+                    message.Abandon();
+                }
+                Console.WriteLine("");
+            });
+
+            Console.Read();
+
+        }
+
+        private static void initSub()
+        {
+            NamespaceManager nm = NamespaceManager.CreateFromConnectionString(connStr);
+            if (!nm.SubscriptionExists(TopicName, SubscriptionName))
+                nm.CreateSubscription(TopicName, SubscriptionName);
+        }
+    }
+}
